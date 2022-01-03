825ec3037234a0dbba3d1103993f5449630e9317
ageneric <55108448+ageneric@users.noreply.github.com>
2022-01-03 18:25:22 +0000

    Add an Editor scene

--- editor.py
+++ editor.py
@@ -2,10 +2,18 @@ import pygame
 import engine.text as text
 import engine.interface as interface
 from engine.base_scene import Scene
-from engine.base_node import Node, SpriteNode, NodeProperties
+from engine.base_node import NodeProperties
 from constants import *
 
-from tree_tab import TreeTabGrid
+from tree_tab import TreeTab
 
-class Editor(TreeTab):
+class Editor(Scene):
+    """The editor that enables the user to edit a scene."""
+    def __init__(self, screen, clock, user_scene):
+        super().__init__(screen, clock)
 
+        self.user_scene = user_scene(screen, clock)
+        self.tree_tab = TreeTab(self.user_scene)
+
+    def update(self):
+        super().update()
